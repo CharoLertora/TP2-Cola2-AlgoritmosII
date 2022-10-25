f1f436017f56d7517df340491fedc67c9cfbd185
belu <bcerrotti@fi.uba.ar>
2022-10-24 23:47:10 -0300

    agrego funciones encargadas de leer archivo y completar lista

---
+++ sistema.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <fstream>
+#include "Lista.h"
+#include "Animal.h"
+#include "Perro.h"
+#include "Gato.h"
+#include "Caballo.h"
+#include "Roedor.h"
+#include "Conejo.h"
+#include "Erizo.h"
+#include "Lagartija.h"
+
+using namespace std;
+
+const string ARCHIVO_ANIMALES = "animales.csv";
+
+const string INICIAL_PERRO = "P";
+const string INICIAL_GATO = "G";
+const string INICIAL_CABALLO = "C";
+const string INICIAL_ROEDOR = "R";
+const string INICIAL_CONEJO = "C";
+const string INICIAL_ERIZO = "E";
+const string INICIAL_LAGARTIJA = "L";
+
+const string ESPECIE_PERRO = "Perro";
+const string ESPECIE_GATO = "Gato";
+const string ESPECIE_CABALLO = "Caballo";
+const string ESPECIE_ROEDOR = "Roedor";
+const string ESPECIE_CONEJO = "Conejo";
+const string ESPECIE_ERIZO = "Erizo";
+const string ESPECIE_LAGARTIJA = "Lagartija";
+
+void agregar_animal(Lista &lista, string nombre, int edad, string tamanio, string especie, string personalidad){
+   
+    if (especie == INICIAL_PERRO){
+        especie = ESPECIE_PERRO;
+        Perro* perro = new Perro(nombre, edad, tamanio, especie, personalidad);
+        lista.alta(perro, 1);
+    }else if (especie == INICIAL_GATO){
+        especie = ESPECIE_GATO;
+        Gato* gato = new Gato(nombre, edad, tamanio, especie, personalidad);
+        lista.alta(gato, 1);
+    }else if (especie == INICIAL_CABALLO){
+        especie = ESPECIE_CABALLO;
+        Caballo* caballo = new Caballo(nombre, edad, tamanio, especie, personalidad);
+        lista.alta(caballo, 1);
+    }else if (especie == INICIAL_ROEDOR){
+        especie = ESPECIE_ROEDOR;
+        Roedor* roedor = new Roedor(nombre, edad, tamanio, especie, personalidad);
+        lista.alta(roedor, 1);
+    }else if (especie == INICIAL_CONEJO){
+        especie = ESPECIE_CONEJO;
+        Conejo* conejo = new Conejo(nombre, edad, tamanio, especie, personalidad);
+        lista.alta(conejo, 1);
+    }else if (especie == INICIAL_ERIZO){
+        especie = ESPECIE_ERIZO;
+        Erizo* erizo = new Erizo(nombre, edad, tamanio, especie, personalidad);
+        lista.alta(erizo, 1);
+    }else {
+        especie = ESPECIE_LAGARTIJA;
+        Lagartija* lagartija = new Lagartija(nombre, edad, tamanio, especie, personalidad);
+        lista.alta(lagartija, 1);
+    }
+}
+
+void llenar_lista (Lista &lista){
+
+    ifstream archivo_animales(ARCHIVO_ANIMALES);
+    
+    if (!archivo_animales.is_open()){
+        cout << "No se ha encontrado un archivo, se creará uno llamado: " << ARCHIVO_ANIMALES << endl;
+        archivo_animales.open(ARCHIVO_ANIMALES, ios::out);
+        archivo_animales.close();
+        archivo_animales.open(ARCHIVO_ANIMALES, ios::in);
+    }
+
+    string nombre, edad, tamanio, especie, personalidad;
+    int num_edad;
+
+    while (getline (archivo_animales, nombre, ',')){ 
+        getline (archivo_animales, edad, ',');
+        getline (archivo_animales, tamanio, ',');
+        getline (archivo_animales, especie, ',');
+        getline (archivo_animales, personalidad);
+        num_edad = stoi(edad);
+        agregar_animal(lista, nombre, num_edad, tamanio, especie, personalidad);
+    }
+    archivo_animales.close();
+}
