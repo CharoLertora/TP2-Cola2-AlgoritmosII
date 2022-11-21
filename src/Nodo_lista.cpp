13cd8c0dc188172ed2b2f65d5654bc6f788512da
belu <bcerrotti@fi.uba.ar>
2022-11-20 22:46:09 -0300

    agrego lista y nodo_lista

---
+++ src/Nodo_lista.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "../include/Nodo_lista.hpp"
+
+using namespace std;
+
+Nodo::Nodo(Animal* animal) {
+    this->animal = animal;
+    siguiente = 0;
+}
+
+Animal* Nodo::obtener_animal() {
+    return animal;
+}
+
+Nodo* Nodo::obtener_siguiente() {
+    return siguiente;
+}
+
+void Nodo::asignar_siguiente(Nodo* s) {
+    siguiente = s;
+}
+
+Nodo::~Nodo () {
+    delete animal;
+}
