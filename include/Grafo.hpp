d28878f408f0b92d9cf82eccd8794741059c63dc
belu <bcerrotti@fi.uba.ar>
2022-12-04 14:33:41 -0300

    cambios grafo

--- include/Grafo.hpp
+++ include/Grafo.hpp
@@ -1,28 +1,19 @@
 #ifndef _GRAFO_HPP_
 #define _GRAFO_HPP_
-#include "Vertice.hpp"
-#include "Lista.hpp"
 
 const int VACIO = 0;
 
 class Grafo {
     //Atributos:
 private:
-    Lista *vertices; 
-    int cant_vertices;
+    
 
     //Métodos:
 public:
 
-    Grafo();
+    //Grafo();
 
-    void agregar_vertice(int fila, int columna, string tipo_terreno);
-
-    Lista* obtener_vertices();
-    
-    Vertice* encontrar_vertice(int fila, int columna);
-
-    ~Grafo();
+    //~Grafo();
 };
 
 #endif //_GRAFO_HPP_
